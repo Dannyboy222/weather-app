6950a4d63cac3618694ea360ef59dcf8005e90de
Danny <ljw5040@gmail.com>
2021-12-08 13:49:36 -0800

    finished js part

---
+++ script.js
@@ -0,0 +1,53 @@
+const weather = {
+	apiKey: 'ee39834b04692da9fb50d283accaf823',
+	fetchWeather: function (city) {
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=' +
+				city +
+				'&units=metric&appid=' +
+				this.apiKey
+		)
+			.then((response) => {
+				if (!response.ok) {
+					alert("I can't find your city");
+					throw new Error("I can't find your city");
+				}
+
+				return response.json();
+			})
+			.then((data) => this.displayWeather(data));
+	},
+	displayWeather: function (data) {
+		const { name } = data;
+		const { icon, description } = data.weather[0];
+		const { temp, humidity } = data.main;
+		const { speed } = data.wind;
+		document.querySelector('.city').innerText = ' Weather in ' + name;
+		document.querySelector('.icon').scr = document.querySelector('.icon').src =
+			'https://openweathermap.org/img/wn/' + icon + '.png';
+		document.querySelector('.description').innerText = description;
+		document.querySelector('.temp').innerText = temp + '°C';
+		document.querySelector('.humidity').innerText =
+			'Humidity: ' + humidity + '%';
+		document.querySelector('.wind').innerText = speed + 'km/h';
+		document.querySelector('.weather').classList.remove('loading');
+		document.body.style.backgroundImage =
+			"url('http://source.unsplash.com/1600x900/?" + name + "')";
+	},
+	search: function () {
+		this.fetchWeather(document.querySelector('.searchBar').value);
+	},
+};
+
+document.querySelector('button').addEventListener('click', function () {
+	weather.search();
+});
+document
+	.querySelector('.searchBar')
+	.addEventListener('keyup', function (event) {
+		if (event.key === 'Enter') {
+			weather.search();
+		}
+	});
+
+weather.fetchWeather('vancouver');
